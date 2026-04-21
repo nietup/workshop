fb50344f060a3cb7855dc076655c2ac7144cff0e
jnie <jnie@netcompany.com>
2026-04-21 13:51:29 +0200

    clean code update description

--- src/main/java/clean/ReportGenerator.java
+++ src/main/java/clean/ReportGenerator.java
@@ -3,8 +3,8 @@ package clean;
 /*
  Cel ćwiczenia:
 
-Klasa zawiera „długą metodę” do refaktoryzacji (Extract Method, lepsze nazewnictwo, stałe zamiast magicznych liczb,
-guard clauses, opcjonalnie Introduce Parameter Object dla ustawień raportu).
+Klasa zawiera długą metodę do refaktoryzacji (Extract Method, lepsze nazewnictwo, stałe zamiast magicznych liczb,
+guard clauses, wydzielenie Parameter Object dla ustawień raportu itp).
 Utrzymaj zachowanie (uruchom LongMethodApp i testuj ręcznie).
  Wejście: "SALES:100;RETURN:20;SALES:50"
  Parametry: timezone (np. "UTC"), locale (np. "PL" / "EN")
